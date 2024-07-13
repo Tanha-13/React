5839f7552434daaaa4d7e6305f31300cf90cd5c1
Tanha <nusrattanha897@gmail.com>
2024-07-14 00:08:56 +0600

    simple counting in js

--- counter-project-js/main.js
+++ counter-project-js/main.js
@@ -1,8 +1,27 @@
 import './style.css'
 
+let counter = 0;
+
+
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Creating a counter project using JavaScript and Vite</h1>
-      <button id="counter" type="button"></button>
+      <p id="value">The value is ${counter}</p>
+      <button id="increase-btn" type="button">Count Up</button>
+      <button id="decrease-btn" type="button">Count Down</button>
   </div>
 `
+
+function increaseCount(){
+  counter++;
+  document.querySelector("#value").innerHTML = `The value is ${counter}`
+}
+function decreaseCount(){
+  if(!counter <= 0){
+    counter--;
+  }
+  document.querySelector("#value").innerHTML = `The value is ${counter}`
+}
+
+document.querySelector("#increase-btn").addEventListener('click', increaseCount);
+document.querySelector("#decrease-btn").addEventListener('click', decreaseCount);
