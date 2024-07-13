a8906cbec5a54b1a854351be43da655c689bdf85
Tanha <nusrattanha897@gmail.com>
2024-07-13 23:42:53 +0600

    setup for a counter project in js via vite

---
+++ counter-project-js/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Creating a counter project using JavaScript and Vite</h1>
+      <button id="counter" type="button"></button>
+  </div>
+`
