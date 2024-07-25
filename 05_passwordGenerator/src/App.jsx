4a43093a8facb96862759b25d35714abc04bf72f
Tanha <nusrattanha897@gmail.com>
2024-07-25 23:45:42 +0600

    concept of useCallBack hook in react

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  // useCallBack(function, [dependencies]) - cache the function between re-renders.
+  const passwordGenerator = useCallback(() => {
+
+
+  }, [length,numberAllowed,charAllowed,setPassword])
   return (
     <>
       <h1 className="text-5xl text-center font-semibold mt-4">Password Generator</h1>
-      {length}
     </>
   )
 }
