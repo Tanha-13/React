2fa61b28112c9cc9dd64c2ca1214eab6ec08d5a7
Tanha <nusrattanha897@gmail.com>
2024-07-29 23:51:14 +0600

    how to create custom hook

--- 06_currency-converter/src/App.jsx
+++ 06_currency-converter/src/App.jsx
@@ -1,15 +1,11 @@
-
-
 function App() {
-  
-
   return (
     <>
-      
-      <h1>Current Converter</h1>
-      
+      <h1 className="text-4xl text-center bg-orange-500 p-2">
+        Current Converter
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
