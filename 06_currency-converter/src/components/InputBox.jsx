66dadebcfc5ea0706ecca0c200cc1e7d65bbfa51
Tanha <nusrattanha897@gmail.com>
2024-07-30 00:25:30 +0600

    reusable input component

---
+++ 06_currency-converter/src/components/InputBox.jsx
@@ -0,0 +1,60 @@
+// import React from "react";
+import PropTypes from "prop-types";
+
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
+  className = "",
+}) {
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <input
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
+        ></select>
+        {
+            currencyOptions.map((currency,index) => (
+                <option key={index} value={`${currency}`}>{currency}</option>
+            ))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default InputBox;
+
+InputBox.propTypes = {
+  label: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  onAmountChange: PropTypes.string.isRequired,
+  onCurrencyChange: PropTypes.string.isRequired,
+  currencyOptions: PropTypes.array.isRequired,
+  selectCurrency: PropTypes.string.isRequired,
+  amountDisable: PropTypes.bool.isRequired,
+  currencyDisable: PropTypes.bool.isRequired,
+};
