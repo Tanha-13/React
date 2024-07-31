bb6978ef7ad380a9fb75f25f091dc68b8ec999ba
Tanha <nusrattanha897@gmail.com>
2024-07-31 22:55:40 +0600

    footer and home component updated

--- 07_react-rounter/src/App.jsx
+++ 07_react-rounter/src/App.jsx
@@ -1,7 +1,15 @@
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+
 function App() {
   return (
     <>
       <h1 className="text-5xl text-center bg-emerald-500 p-4">React Router</h1>
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+
     </>
   );
 }
