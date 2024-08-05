4ab084210c7c6d3927f83133cb75abfb2dcd3ac5
Tanha <nusrattanha897@gmail.com>
2024-08-06 01:13:10 +0600

    creating context using different approach

--- 09_theme-switcher/src/App.jsx
+++ 09_theme-switcher/src/App.jsx
@@ -1,7 +1,19 @@
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-center">Theme Switcher</h1>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+
+            {/* themeBtn */}
+
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* Card */}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
